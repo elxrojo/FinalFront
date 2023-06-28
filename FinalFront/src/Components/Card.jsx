a56088a06ea23089f78ba09626fff595bff24f3a
elxro <monetaaaaas@gmail.com>
2023-06-28 08:37:17 -0300

    Agregando details (bug)

--- FinalFront/src/Components/Card.jsx
+++ FinalFront/src/Components/Card.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Card({userDetail,likeFunct,id}){
     return (
-        <div>
+        <Link to={`details/${id}`}>
             <img src="./src/asset/user.png" alt="imagen usuario" />
             <h2>{userDetail.name}</h2>
             <h3>{userDetail.email}</h3>
             <p>{userDetail.company.name}</p>
             <button onClick={()=>likeFunct(id)}>Guardar</button>
-        </div>
+        </Link>
     )
 }
--- FinalFront/src/Components/Detalle.jsx
+++ FinalFront/src/Components/Detalle.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 export default function Detalle(){
-    return
+    const [odontoDetails, setOdontoDetails] = useState('')
+    const {id} = useParams()
+
+    async function fetching(){
+        const odontoObtenido = await (await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)).json()
+        console.log(odontoObtenido);
+        setOdontoDetails(odontoObtenido)
+    }
+
+    useEffect(() => {
+        fetching()
+    },[])
+
+    return(
+        <div>
+            <h1>{odontoDetails.name}</h1>
+            <h3>{odontoDetails.website}</h3>
+            <h3>{odontoDetails.email}</h3>
+            <h3>{odontoDetails.phone}</h3>
+            {/* <div>
+                <p>Trabaja en:</p>
+                <p>{odontoDetails.company.name}</p>
+            </div>
+            <div>
+                <p>Ciudad:</p>
+                <p>{odontoDetails.address.city}</p>
+            </div> */}
+
+        </div>
+    )
 }
+
+// ! Resolver problema, no se puede renderizar el objeto de un objeto
