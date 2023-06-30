6a1280a78f2a4911ecc347a79404c68403ff654b
elxro <monetaaaaas@gmail.com>
2023-06-30 08:30:44 -0300

    Terminando Estilos

--- FinalFront/src/Components/Contacto.jsx
+++ FinalFront/src/Components/Contacto.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { toast } from 'sonner'
+import styles from '../Styles/ComponentStyles/Contacto.module.css'
+import { ThemeContext } from "../Contexts/ThemeContext";
 
 export default function Contacto(){
 
@@ -9,6 +11,8 @@ export default function Contacto(){
     const [correo, setCorreo] = useState('')
     const [errores, setErrores] = useState([])
     const [validado, setValidado] = useState(false);
+
+    const {theme} = useContext(ThemeContext)
 
 
     function nombreEstaBien() {
@@ -42,25 +46,44 @@ export default function Contacto(){
         });
     
         if (!errores.some((error) => error) && validado) {
-            toast.success('¡Funciona!');
+            toast.success('Datos enviados correctamente');
         }
     }, [errores, validado]);
 
     return (
-        <form onSubmit={(e) => e.preventDefault()} noValidate>
-            <label>Nombre</label>
-            <input type="text"
-            value={nombre}
-            onChange={(e)=> setNombre(e.target.value)}
-            />
+        <div className={styles.contenedor}>
+            <form onSubmit={(e) => e.preventDefault()} noValidate className={styles.form} style={{background:theme.color1, color:theme.colorLetra}}>
+                <h1>Contacta con nosotros!</h1>
+                    <div className={styles.inputs}>
+                        <div className={styles.input}>
+                            <div className={styles.cajita}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill='black' className="bi bi-person" viewBox="0 0 16 16">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                                </svg>
+                            </div>
+                            <input type="text"
+                            value={nombre}
+                            onChange={(e)=> setNombre(e.target.value)}
+                            placeholder="Nombre"
+                            />
+                        </div>
 
-            <label>Email</label>
-            <input type="email"
-            value={correo}
-            onChange={(e)=> setCorreo(e.target.value)}
-            />
+                        <div className={styles.input}>
+                            <div className={styles.cajita}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" className="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                                </svg>
+                            </div>
+                            <input type="email"
+                            value={correo}
+                            onChange={(e)=> setCorreo(e.target.value)}
+                            placeholder="Correo"
+                            />
+                        </div>
+                </div>
 
-            <button onClick={mostrarErrores}>Enviar datita</button>
-        </form>
+                <button onClick={mostrarErrores}>Enviar datos</button>
+            </form>
+        </div>
     )
 }
--- FinalFront/src/Components/Favoritos.jsx
+++ FinalFront/src/Components/Favoritos.jsx
@@ -1,17 +1,18 @@
 import { OdontoContext } from "../Contexts/OdontoContext"
 import CardList from "./CardList"
 import { useContext} from "react"
+import styles from "../Styles/ComponentStyles/Favs.module.css"
 
 export default function Favoritos(){
     const {favsOdontosLS} = useContext(OdontoContext)
 
     return( 
-        <>
+        <div className={styles.favs}>
             {favsOdontosLS.length > 0 ?
             <CardList odontos={favsOdontosLS}/>
             :
             <h1>Aun no has agregado a tus odontologos favoritos!</h1>
             }
-        </>
+        </div>
     )
 }
